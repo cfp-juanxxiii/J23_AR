1b22cd32cb27a3ca95d0e97cafd8d8c77b065dae
Fernando Paniagua <fernando.paniagua@gmail.com>
2018-12-01 15:10:24 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/J23Generator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J23Generator : MonoBehaviour {
+    public GameObject prefabJ23;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Generar", 1,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Generar()
+    {
+        Instantiate(prefabJ23, transform);
+    }
+}
---
+++ Assets/_GameAssets/Scripts/VideoControllerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoControllerScript : MonoBehaviour {
+    public VideoPlayer vp;
+    public GameObject firstFrame;
+    private bool firstTime=true;
+
+    private void Start()
+    {
+        vp.frame = 1;
+    }
+
+    void Update () {
+        if (Input.touches.Length > 0 && Input.touches[0].phase==TouchPhase.Began)
+        {
+            GameObject go = GetTouchedGO();
+            ProcesarGO(go);
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = GetClickedGO();
+            ProcesarGO(go);
+        }
+    }
+
+    private void ProcesarGO(GameObject go)
+    {
+        if (go.name == "VideoController")
+        {
+            if (!vp.isPlaying)
+            {
+                if (firstTime)
+                {
+                    firstTime = false;
+                    Destroy(firstFrame);
+                }
+                vp.Play();
+            }
+            else
+            {
+                vp.Pause();
+            }
+        }
+    }
+
+    private GameObject GetTouchedGO()
+    {
+        GameObject touchedGO = null;
+        Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(raycast, out raycastHit))
+        {
+            touchedGO = raycastHit.transform.gameObject;
+        }
+        return touchedGO;
+    }
+
+    private GameObject GetClickedGO()
+    {
+        GameObject touchedGO = null;
+        Ray raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(raycast, out raycastHit))
+        {
+            touchedGO = raycastHit.transform.gameObject;
+        }
+        return touchedGO;
+    }
+}
